a38e34b0a3b76a852a8485931fba93b1efed5fff
VKrishna2090 <vkrishna2090@gmail.com>
2022-03-28 11:58:21 +0530

    🤡 Secured the API

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from chat import get_response
 
 from flask_cors import CORS
 
+import os
+
 app = Flask(__name__)
 CORS(app)
 
@@ -13,11 +15,13 @@ def index_get():
 
 @app.post('/predict')
 def predict():
-    text = request.get_json().get("message") 
-    # TODO: Check if text is valid
-    response = get_response(text)
-    message = {"answer": response}
-    return jsonify(message)
+    if request.referrer == os.environ.get('AUTHORIZED_LINK'):
+        text = request.get_json().get("message") 
+        response = get_response(text)
+        message = {"answer": response}
+        return jsonify(message)
+    else:
+        return jsonify({"code":400,"answer": "Unauthorised access"})
 
 if __name__ == "__main__":
     app.run(debug=True)
